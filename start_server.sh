20d744986a3f93aae2e97a37f042cf73d086e776
Divyansh <divyanshsingh1800@gmail.com>
2025-04-17 11:06:09 +0100

    Update file permissions after nohup attempt

--- start_server.sh
+++ start_server.sh
@@ -6,5 +6,5 @@ cd "/Users/divyanshsingh/Desktop/project copy"
 # Activate virtual environment
 source venv/bin/activate
 
-# Start gunicorn
-exec gunicorn --workers 1 --bind 0.0.0.0:5000 "app:create_app()" 
+# Start gunicorn with logging to terminal
+exec gunicorn --workers 1 --bind 0.0.0.0:5000 "app:create_app()" --log-level debug --access-logfile - --error-logfile - 
